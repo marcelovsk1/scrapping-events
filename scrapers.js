8ffc20dd12ab0f87de1af4818ae534971ec264fa
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-24 15:45:09 -0300

    working on

--- scrapers.js
+++ scrapers.js
@@ -7,26 +7,26 @@ async function scrapeProduct(url) {
   page.goto(url);
   await page.waitForNavigation();
 
-  const [el] = await page.$x('/html/body/div[2]/div/div/div[1]/div/div/div/h1');
+  // const [el] = await page.$x('/html/body/div[2]/div/div/div[1]/div/div/div/h1');
+  // const txt = await el.getProperty('textContent');
+  // const webPage = await txt.jsonValue();
+
+  const [el] = await page.$x('//*[@id="event_list"]/div[2]/ul/div[1]/li[9]/div[4]/div/div[2]/div/div/a/h3');
   const txt = await el.getProperty('textContent');
-  const webPage = await txt.jsonValue();
+  const eventTitle = await txt.jsonValue();
 
-  const [el1] = await page.$x('//*[@id="all"]/li[7]/div[2]/div[2]/h4/a');
-  const txt1 = await el1.getProperty('textContent');
-  const eventTitle = await txt1.jsonValue();
+  // const [el1] = await page.$x('//*[@id="event_description"]');
+  // const txt1 = await el1.getProperty('textContent');
+  // const eventDescription = await txt1.jsonValue();
 
-  const [el2] = await page.$x('//*[@id="all"]/li[7]/div[2]/div[2]/p/text()');
-  const txt2 = await el2.getProperty('textContent');
-  const eventDescription = await txt2.jsonValue();
-
-  const [el3] = await page.$x('//*[@id="all"]/li[7]/div[1]/a/img');
-  const src = await el3.getProperty('src');
+  const [el2] = await page.$x('//*[@id="event_list"]/div[2]/ul/div[1]/li[9]/div[3]/img');
+  const src = await el2.getProperty('src');
   const imageURL = await src.jsonValue();
 
-  console.log({webPage, eventTitle, eventDescription, imageURL});
+  console.log({eventTitle, imageURL});
 
   browser.close();
 
 };
 
-scrapeProduct('http://www.go-montreal.com/attraction_events_nov.htm');
+scrapeProduct('https://allevents.in/montreal/sports?ref=cityhome-category-box-new');
