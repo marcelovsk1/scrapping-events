7dd13a22fd9cf440874a168fc8cc37bf18bdf16a
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-24 16:22:55 -0300

    working on

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 const axios = require('axios')
 const cheerio = require('cheerio')
+
+const fetchEvents = async () => {
+  try {
+    await response = axios.get('http://www.go-montreal.com/attraction_events_dec.htm')
+  } catch (err) {
+    console.error(err)
+  }
+}
