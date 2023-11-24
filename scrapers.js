2e9272eb6dbf8322bc9da1c3a80300ebac5867cb
Marcelo Amaral Alves <marceloaa@live.com>
2023-11-24 07:19:34 -0300

    working on

--- scrapers.js
+++ scrapers.js
@@ -11,23 +11,19 @@ async function scrapeProduct(url) {
   const txt = await el.getProperty('textContent');
   const webPage = await txt.jsonValue();
 
-  const [el1] = await page.$x('//*[@id="all"]/li[8]/div[2]/div[2]/h4/a');
+  const [el1] = await page.$x('//*[@id="all"]/li[7]/div[2]/div[2]/h4/a');
   const txt1 = await el1.getProperty('textContent');
   const eventTitle = await txt1.jsonValue();
 
-  const [el2] = await page.$x('//*[@id="all"]/li[7]/div[2]/div[1]/div/div');
+  const [el2] = await page.$x('//*[@id="all"]/li[7]/div[2]/div[2]/p/text()');
   const txt2 = await el2.getProperty('textContent');
-  const eventDate = await txt2.jsonValue();
+  const eventDescription = await txt2.jsonValue();
 
-  const [el3] = await page.$x('//*[@id="all"]/li[8]/div[2]/div[2]/p/text()');
-  const txt3 = await el3.getProperty('textContent');
-  const eventDescription = await txt3.jsonValue();
-
-  const [el4] = await page.$x('//*[@id="all"]/li[8]/div[1]/a/img');
-  const src = await el4.getProperty('src');
+  const [el3] = await page.$x('//*[@id="all"]/li[7]/div[1]/a/img');
+  const src = await el3.getProperty('src');
   const imageURL = await src.jsonValue();
 
-  console.log({webPage, eventTitle, eventDate, eventDescription, imageURL});
+  console.log({webPage, eventTitle, eventDescription, imageURL});
 
   browser.close();
 
